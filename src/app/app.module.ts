c5c46732f5e4e84354e518e13d5594a0ccf8b460
Olga Gerus <1979.olga.gerus@gmail.com>
2024-06-13 22:15:43 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,12 @@ import {RecipesComponent} from './recipes/recipes.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
 import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
 import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
-import {DropdownDirective} from "./shared/dropdown.directive";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RecipeService} from "./recipes/resipe.service";
 import {HttpClientModule} from "@angular/common/http";
-import {SigninComponent} from './auth/signin/signin.component';
-import {SignupComponent} from './auth/signup/signup.component';
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 
 @NgModule({
@@ -26,12 +23,9 @@ import {ShoppingListModule} from "./shopping-list/shopping-list.module";
     RecipeDetailComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    DropdownDirective,
     RecipeStartComponent,
-    RecipeEditComponent,
-    SigninComponent,
-    SignupComponent
-  ],
+    RecipeEditComponent
+      ],
   imports: [
     BrowserModule,
     AppRoutingModule,
